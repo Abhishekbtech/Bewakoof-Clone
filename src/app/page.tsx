e84c6a9fb041283db7f59d2f201120fcfafe4bd8
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-02-12 17:32:29 +0530

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col sm:grid sm:grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div >
       <h1>HEloo</h1>
     </div>
   );
